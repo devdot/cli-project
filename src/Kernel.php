05bc7dd7b171baaf192a64e78452a6e122038f76
Thomas Kuschan <mail@thomaskuschan.de>
2024-08-09 09:55:20 +0200

    Add stubs for kernel arrays

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,6 +6,20 @@ use Devdot\Cli\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
+    /**
+     * @var class-string[]
+     */
+    protected array $services = [
+        //
+    ];
+
+    /**
+     * @var class-string<\Devdot\Cli\Container\ServiceProvider>[]
+     */
+    protected array $providers = [
+        //
+    ];
+
     public function __construct(string $dir = __DIR__, string $namespace = __NAMESPACE__)
     {
         parent::__construct($dir, $namespace);
